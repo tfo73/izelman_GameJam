f66210bc2461227c91122c20af2e40a718c5816a
tfo73 <bora01fazli@gmail.com>
2023-08-22 17:11:39 +0300

    kamera yaptım

---
+++ İzlemanGameJam/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float FollowSpeed, yOffset;
+    public Transform target;
+
+    void Update()
+    {
+        Vector3 newPos = new Vector3(target.position.x,target.position.y + yOffset,-10f);
+        transform.position = Vector3.Slerp(transform.position,newPos,FollowSpeed*Time.deltaTime);
+    }
+}
